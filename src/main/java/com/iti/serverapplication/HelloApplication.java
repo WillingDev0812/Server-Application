f365795b33d23be751602b2748e5b5ab1aab0222
WillingDev0812 <greendev1004@gmail.com>
2024-07-23 19:25:19 +0300

    Database Created.

--- src/main/java/com/iti/serverapplication/HelloApplication.java
+++ src/main/java/com/iti/serverapplication/HelloApplication.java
@@ -11,8 +11,9 @@ public class HelloApplication extends Application {
     @Override
     public void start(Stage stage) throws IOException {
         FXMLLoader fxmlLoader = new FXMLLoader(HelloApplication.class.getResource("/com/iti/serverapplication/Server.fxml"));
-        Scene scene = new Scene(fxmlLoader.load(), 1920, 1100);
-        stage.setTitle("Hello!");
+        Scene scene = new Scene(fxmlLoader.load());
+        stage.setFullScreen(true);
+        stage.setTitle("Server Application");
         stage.setScene(scene);
         stage.show();
     }
