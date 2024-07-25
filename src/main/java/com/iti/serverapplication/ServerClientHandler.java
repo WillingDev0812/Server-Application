c074eac456682a47673ecef9c2ed9abc9d25577e
WillingDev0812 <greendev1004@gmail.com>
2024-07-26 01:30:35 +0300

    Lists of users can now be avalible except my current user Now user can get to show his username

--- src/main/java/com/iti/serverapplication/ServerClientHandler.java
+++ src/main/java/com/iti/serverapplication/ServerClientHandler.java
@@ -9,6 +9,8 @@ import java.sql.Connection;
 import java.sql.PreparedStatement;
 import java.sql.ResultSet;
 import java.sql.SQLException;
+import java.util.ArrayList;
+import java.util.List;
 
 public class ServerClientHandler implements Runnable {
 
@@ -17,7 +19,7 @@ public class ServerClientHandler implements Runnable {
     public ServerClientHandler(Socket socket) {
         this.socket = socket;
     }
-
+    List<String> users = new ArrayList<>();
     @Override
     public void run() {
         try (DataInputStream input = new DataInputStream(socket.getInputStream());
@@ -26,6 +28,7 @@ public class ServerClientHandler implements Runnable {
             String action = input.readUTF();
             System.out.println("Action received: " + action);
 
+            String username;
             String email;
             String password;
             boolean success;
@@ -33,18 +36,32 @@ public class ServerClientHandler implements Runnable {
             switch (action) {
                 case "login":
                     email = input.readUTF();
+                    //username = input.readUTF();
                     password = input.readUTF();
-                    System.out.println("Login request - Email: " + email);
+                    //System.out.println("Login request - Email: " + email + ", Username: " + username);
                     success = checkLogin(email, password);
                     output.writeBoolean(success);
                     break;
                 case "signup":
-                    String username = input.readUTF();
+                    username = input.readUTF();
                     email = input.readUTF();
                     password = input.readUTF();
                     System.out.println("Sign-up request - Username: " + username + ", Email: " + email);
                     success = registerUser(username, email, password);
                     output.writeBoolean(success);
+                    break;
+                case "showUsers":
+                    // Read email from client before calling getUsernames
+                    email = input.readUTF();
+                    output.writeUTF(getUsername(email)); //send username to client
+                    getUsernames(email);
+                    output.writeInt(users.size());
+                    System.out.println(users.size());
+                    for(int i=0;i<users.size();i++)
+                    {
+                        output.writeUTF(users.get(i));
+                    }
+                    output.flush();
                     break;
                 default:
                     output.writeUTF("Invalid action");
@@ -61,11 +78,11 @@ public class ServerClientHandler implements Runnable {
         }
     }
 
-    private boolean checkLogin(String email, String password) throws SQLException {
+    private boolean checkLogin(String username, String password) throws SQLException {
         Connection connection = DatabaseConnectionManager.getConnection();
         String query = "SELECT * FROM users WHERE email = ? AND password = ?";
         try (PreparedStatement statement = connection.prepareStatement(query)) {
-            statement.setString(1, email);
+            statement.setString(1, username);
             statement.setString(2, password);
             try (ResultSet resultSet = statement.executeQuery()) {
                 return resultSet.next();
@@ -82,5 +99,45 @@ public class ServerClientHandler implements Runnable {
             statement.setString(3, password);
             return statement.executeUpdate() > 0;
         }
+    }
+
+    private void getUsernames(String email) throws SQLException {
+        Connection connection = DatabaseConnectionManager.getConnection();
+        String query = "SELECT username FROM users WHERE email != ?";
+        System.out.println("a7a"+email);
+        try (PreparedStatement statement = connection.prepareStatement(query)) {
+            statement.setString(1, email);
+            try (ResultSet resultSet = statement.executeQuery()) {
+                users.clear();
+                while (resultSet.next()) {
+                    users.add(resultSet.getString("username"));
+                }
+            }
+        } catch (SQLException e) {
+            e.printStackTrace();
+        }
+    }
+    private String getUsername(String email) {
+        String username = "Player"; // Default value
+        Connection connection = null;
+        PreparedStatement statement = null;
+        ResultSet resultSet = null;
+
+        try {
+            connection = DatabaseConnectionManager.getConnection();
+            String query = "SELECT username FROM users WHERE email = ?"; // Use = for comparison
+            statement = connection.prepareStatement(query);
+            statement.setString(1, email);
+            resultSet = statement.executeQuery();
+
+            if (resultSet.next()) {
+                username = resultSet.getString("username");
+            }
+
+        } catch (SQLException e) {
+            System.err.println("Database error: " + e.getMessage());
+            e.printStackTrace();
+        }
+        return username;
     }
 }
